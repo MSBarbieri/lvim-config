1d6dbba9e375b1c0a0a1088318200b1cd7dd36f8
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-02-06 13:17:45 -0300

    add focus

---
+++ lua/user/hotkeys/focus.lua
@@ -0,0 +1,7 @@
+lvim.builtin.which_key.mappings['z'] = {
+  name = 'Focus',
+  z = { '<cmd>FocusMaxOrEqual<cr>', 'Toggle Fullscreen' },
+  r = { '<cmd>FocusToggle<cr>', 'Toggle Auto-Resize' },
+  e = { '<cmd>FocusEqualise<cr>', 'Equalize' },
+}
+lvim.keys.normal_mode['go'] = '<cmd>ObsidianFollowLink<cr>'
